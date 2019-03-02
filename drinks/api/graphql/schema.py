e173c272832c6323bb892041927a321fddf0100d
Francisco Rivera <franmrivera@gmail.com>
2019-03-02 16:45:59 -0300

    graphql single item filter

--- drinks/api/graphql/schema.py
+++ drinks/api/graphql/schema.py
@@ -1,28 +1,42 @@
 from graphene_django import DjangoObjectType
 import graphene
 
-from drinks import models
+from drinks.models import Drink, Ingredient
 
 
-class Drink(DjangoObjectType):
+class DrinkType(DjangoObjectType):
     class Meta:
-        model = models.Drink
+        model = Drink
 
 
-class Ingredient(DjangoObjectType):
+class IngredientType(DjangoObjectType):
     class Meta:
-        model = models.Ingredient
+        model = Ingredient
 
 
 class Query(graphene.ObjectType):
-    drinks = graphene.List(Drink)
-    ingredients = graphene.List(Ingredient)
+    drink = graphene.Field(DrinkType, id=graphene.Int())
+    all_drinks = graphene.List(DrinkType)
+    ingredient = graphene.Field(IngredientType, id=graphene.Int())
+    all_ingredients = graphene.List(IngredientType, id=graphene.Int())
 
-    def resolve_drinks(self, info):
-        return models.Drink.objects.all()
+    def resolve_drink(self, info, **kwargs):
+        drink_id = kwargs.get("id")
 
-    def resolve_ingredients(self, info):
-        return models.Ingredient.objects.all()
+        if drink_id is not None:
+            return Drink.objects.get(pk=drink_id)
+
+    def resolve_all_drinks(self, info):
+        return Drink.objects.all()
+
+    def resolve_ingredient(self, info, **kwargs):
+        ingredient_id = kwargs.get("id")
+
+        if ingredient_id is not None:
+            return Ingredient.objects.get(pk=ingredient_id)
+
+    def resolve_all_ingredients(self, info):
+        return Ingredient.objects.all()
 
 
 schema = graphene.Schema(query=Query)
