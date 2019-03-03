f42bd2be9d2916aa5d15ccabe74553d9d2b34a96
Francisco Rivera <franmrivera@gmail.com>
2019-03-02 22:11:38 -0300

    missed test file

---
+++ drinks/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from drinks.models import Drink, Ingredient
+
+
+class TemplateViewTests(TestCase):
+
+    def setUp(self):
+        self.whisky = Ingredient.objects.create(name='Whisky')
+        self.coca_cola = Ingredient.objects.create(name='Coca Cola')
+        self.whiscola = Drink.objects.create(name='Whiscola')
+        self.whiscola.ingredients.add(self.whisky, self.coca_cola)
+
+    def test_index(self):
+        url = reverse('template:index')
+        response = self.client.get(url)
+        self.assertContains(response, self.whiscola.id)
+        self.assertContains(response, self.whiscola.name)
+
+    def test_detail(self):
+        url = reverse('template:detail', args=(self.whiscola.id,))
+        response = self.client.get(url)
+        self.assertContains(response, self.whisky.name)
+        self.assertContains(response, self.coca_cola.name)
+        self.assertContains(response, self.whiscola.name)
