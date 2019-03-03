49df090aeec18cad4e9c307c736fa87fd94db821
Francisco Rivera <franmrivera@gmail.com>
2019-03-02 23:11:48 -0300

    url tests

--- drinks/tests.py
+++ drinks/tests.py
@@ -4,22 +4,32 @@ from django.urls import reverse
 from drinks.models import Drink, Ingredient
 
 
-class TemplateViewTests(TestCase):
-
+class UrlTests(TestCase):
     def setUp(self):
-        self.whisky = Ingredient.objects.create(name='Whisky')
-        self.coca_cola = Ingredient.objects.create(name='Coca Cola')
-        self.whiscola = Drink.objects.create(name='Whiscola')
+        self.whiscola = Drink.objects.create(name="Whiscola")
+
+    def test_urls(self):
+        self.assertEqual(reverse("template:index"), "/drinks/")
+        self.assertEqual(
+            reverse("template:detail", args=(self.whiscola.id,)), "/drinks/1/"
+        )
+
+
+class TemplateViewTests(TestCase):
+    def setUp(self):
+        self.whisky = Ingredient.objects.create(name="Whisky")
+        self.coca_cola = Ingredient.objects.create(name="Coca Cola")
+        self.whiscola = Drink.objects.create(name="Whiscola")
         self.whiscola.ingredients.add(self.whisky, self.coca_cola)
 
     def test_index(self):
-        url = reverse('template:index')
+        url = reverse("template:index")
         response = self.client.get(url)
         self.assertContains(response, self.whiscola.id)
         self.assertContains(response, self.whiscola.name)
 
     def test_detail(self):
-        url = reverse('template:detail', args=(self.whiscola.id,))
+        url = reverse("template:detail", args=(self.whiscola.id,))
         response = self.client.get(url)
         self.assertContains(response, self.whisky.name)
         self.assertContains(response, self.coca_cola.name)
--- drinks/urls.py
+++ drinks/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from drinks.views import IndexView, DetailView
 
-drinks_templates_urlpatterns = ([
-    path("drinks/<int:pk>/", DetailView.as_view(), name="detail"),
-    path("drinks/", IndexView.as_view(), name="index"),
-], 'drinks')
+drinks_templates_urlpatterns = (
+    [
+        path("drinks/<int:pk>/", DetailView.as_view(), name="detail"),
+        path("drinks/", IndexView.as_view(), name="index"),
+    ],
+    "drinks",
+)
