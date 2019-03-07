dc6eea3c54423af1381f356e79c5aac5c5a78b9b
Francisco Rivera <franmrivera@gmail.com>
2019-03-06 23:13:24 -0300

    get/create graphql tests

--- drinks/api/graphql/urls.py
+++ drinks/api/graphql/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from graphene_django.views import GraphQLView
 
-graphql_urlpatterns = [path("", GraphQLView.as_view(graphiql=True))]
+graphql_urlpatterns = (
+    [path("", GraphQLView.as_view(graphiql=True), name="api")],
+    "drinks",
+)
