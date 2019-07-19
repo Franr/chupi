53df0baf30e2b30ae9fd92af28c3f8fb823d1c31
Francisco Rivera <franmrivera@gmail.com>
2019-07-19 12:58:31 -0300

    JWT Authentication + improved readme

--- drinks/api/rest/routers.py
+++ drinks/api/rest/routers.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
 
 from drinks.api.rest.views import DrinkViewSet, IngredientViewSet
 
@@ -8,4 +9,15 @@ drinks_router.register("drinks", DrinkViewSet, base_name="rest-drinks")
 drinks_router.register("ingredients", IngredientViewSet, base_name="rest-ingredients")
 
 
-rest_urlpatterns = ([path("", include(drinks_router.urls))], "drinks")
+rest_urlpatterns = (
+    [
+        path("", include(drinks_router.urls)),
+        path(
+            "token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"
+        ),
+        path(
+            "token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"
+        ),
+    ],
+    "drinks",
+)
